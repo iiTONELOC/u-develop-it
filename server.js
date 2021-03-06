bb6bb3b976c70e404ae24cae43b1031e17939365
iiTONELOC <anthonytropeano@me.com>
2021-03-05 20:21:01 -0500

    added the voter routes

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
 const db = require('./db/database');
 const PORT = process.env.PORT || 3001;
 const app = express();
